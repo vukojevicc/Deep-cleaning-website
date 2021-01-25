5052db9c2052b39c46654c740f1598dd060302a7
aleksandar <aleksandar.vukojevicc@gmail.com>
2021-01-25 13:55:29 +0100

    applied smooth scroll behavior via jQuery

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+          event.preventDefault();
+          var hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, 'swing', function(){
+            window.location.hash = hash;
+          });
+        }
+      });
     function myFunction(x) {
         var j = document.getElementsByClassName("dolazak-na-adresu");
         var k = document.getElementsByClassName("js");
